f99c157567856e5613b492d903d5c5083b3396fe
Marcis Deguts <blitboi303@gmail.com>
2026-07-03 09:22:13 +0300

    Color scheme, show hidden files in search

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,17 @@
+return {
+  { "folke/tokyonight.nvim" },
+  { "catppuccin/nvim", name = "catppuccin" },
+  { "ellisonleao/gruvbox.nvim" },
+  { "rebelot/kanagawa.nvim" },
+  { "rose-pine/neovim", name = "rose-pine" },
+  { "EdenEast/nightfox.nvim" },
+  { "sainnhe/everforest" },
+  { "navarasu/onedark.nvim" },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
