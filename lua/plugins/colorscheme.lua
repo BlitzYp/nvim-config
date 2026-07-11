19399fb42590a464b6a08278a01213cfdafc2011
marcisd1 <marcisd456@gmail.com>
2026-07-11 16:18:12 +0300

    Colorscheme update

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,17 +1,17 @@
 return {
   { "folke/tokyonight.nvim" },
   { "catppuccin/nvim", name = "catppuccin" },
-  { "ellisonleao/gruvbox.nvim" },
-  { "rebelot/kanagawa.nvim" },
+  { "ellisonleao/gruvbox.nvim", name="gruvbox" },
+  { "rebelot/kanagawa.nvim" , name="kanagawa"},
   { "rose-pine/neovim", name = "rose-pine" },
-  { "EdenEast/nightfox.nvim" },
-  { "sainnhe/everforest" },
-  { "navarasu/onedark.nvim" },
+  { "EdenEast/nightfox.nvim", name="nightfox" },
+  { "sainnhe/everforest", name="everforest"},
+  { "navarasu/onedark.nvim", name="onedark" },
 
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
+      colorscheme = "gruvbox",
     },
   },
 }
